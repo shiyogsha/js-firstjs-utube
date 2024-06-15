5006184d76cfd0463e6185c112fe7cc6ecd195a1
shivamshahane <shivamshahane2001@gmail.com>
2024-06-16 03:11:47 +0530

    data types

--- .devcontainer/01_basics/basics/02_dataTypes.js
+++ .devcontainer/01_basics/basics/02_dataTypes.js
@@ -1,2 +1,23 @@
-"use strict"; //treat all js code as newer version//
-alert("shivam")
+"use strict"; //treat all js code as newer version
+// alert(3+3) // we are busing nodejs,not browser
+
+console.log(3
+    +5) //code redabilirty must high //
+console.log("shivam")
+
+let name = "shivam" //string//
+let age = 23
+let isLoggedIn = false
+
+//number =>2 to power 53
+//bigint
+//string=>"
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined)
+console.log(typeof null)
